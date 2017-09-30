9f076026b80e48ba142a6786faa9ac4eb9072f22
Lavee Singh <archives987@gmail.com>
2017-10-01 05:13:26 +0530

    example: add another example of action-reducer and action-payloads

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 import { createStore } from 'redux';
 
 // define reducers
-const reducer = function(state={}, action){
+const reducer = function(state=[], action){
   switch(action.type){
     case 'POST_BOOK':
       return state = action.payload;
@@ -16,14 +16,22 @@ const store = createStore(reducer);
 
 store.subscribe(function(){
   console.log('Current State is:', store.getState());
+  console.log('Price of second book: ', store.getState()[1].price);
 })
 
 // create and dispatch actions
 store.dispatch({
   type: 'POST_BOOK',
-  payload: {
-    'title': 'harry potter and philosopher\' stone',
-    'author': 'j. k. rowling',
-    'price': 772
-  }
+  payload: [
+    {
+      'title': 'harry potter and philosopher\' stone',
+      'author': 'j. k. rowling',
+      'price': 772
+    },
+    {
+      'title': 'harry potter and the chamber of secrets',
+      'author': 'j. k. rowling',
+      'price': 800
+    }
+  ]
 })
