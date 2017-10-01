2e3ad20836df39b97cfa2f65027028fbf032e275
Lavee Singh <archives987@gmail.com>
2017-10-02 04:27:34 +0530

    feat: add redux-logger as middleware for logging

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 'use strict'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
 import reducers from './reducers/index'
 import { addToCart } from './actions/cartActions'
 import { HP1, HP2, HP3 } from './books'
 import { postBook, updateBook } from './actions/booksActions'
 
 // create store
-const store = createStore(reducers)
-
-store.subscribe(function(){
-  console.log('Current State is:', store.getState())
-})
+const middleware = applyMiddleware(logger)
+const store = createStore(reducers, middleware)
 
 // adding three books
 store.dispatch(postBook([HP1, HP2, HP3]))
