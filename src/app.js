4be5c829a00f6e1b8d49b9af9794f798b0ec41cc
Lavee Singh <archives987@gmail.com>
2017-10-01 06:12:12 +0530

    exmaple: add yet another example of action-reducers and action-payloads

--- src/app.js
+++ src/app.js
@@ -4,10 +4,42 @@ import { createStore } from 'redux'
 // define reducers
 const reducer = function(state={books:[]}, action){
   switch(action.type){
+
     case 'POST_BOOK':
-      let books = state.books.concat(action.payload)
-      return { books }
+      //let books = state.books.concat(action.payload)
+      //return { books }
+      return { books: [...state.books, ...action.payload]}  // ... operator requires babel-preset-stage-1
       break
+
+    case 'DELETE_BOOK':
+      const deleteBooks = [...state.books]
+      const indexToDelete = deleteBooks.findIndex(function(book){
+        return book.id == action.payload.id
+      })
+      return { 
+        books: [
+          ...deleteBooks.slice(0, indexToDelete), 
+          ...deleteBooks.slice(indexToDelete+1)
+        ]
+      }
+      break
+
+    case 'UPDATE_BOOK':
+      const updateBooks = [...state.books]
+      const indexToUpdate = updateBooks.findIndex(function(book){
+        return book.id == action.payload.id
+      })
+      const newBook = {
+        ...updateBooks[indexToUpdate],
+        title: action.payload.title
+      }
+      return {
+        books: [
+          ...updateBooks.slice(0, indexToUpdate), newBook,
+          ...updateBooks.slice(indexToUpdate+1)
+        ]
+      }
+
   }
   return state
 }
@@ -25,11 +57,13 @@ store.dispatch({
   type: 'POST_BOOK',
   payload: [
     {
+      'id': 1,
       'title': 'harry potter and philosopher\' stone',
       'author': 'j. k. rowling',
       'price': 772
     },
     {
+      'id': 2,
       'title': 'harry potter and the chamber of secrets',
       'author': 'j. k. rowling',
       'price': 800
@@ -38,9 +72,27 @@ store.dispatch({
 })
 store.dispatch({
   type: 'POST_BOOK',
-  payload: {
+  payload: [
+    {
+      'id': 3,
       'title': 'harry potter and the prisoner of azakaban',
       'author': 'j. k. rowling',
       'price': 650
+    }
+  ]
+})
+
+store.dispatch({
+  type: 'DELETE_BOOK',
+  payload: {
+    'id': 2
+  }
+})
+
+store.dispatch({
+  type: 'UPDATE_BOOK',
+  payload: {
+    'id': 1,
+    'title': 'harry potter and sorcerer\'s stone'
   }
 })
